164d36744dc5eddb2978a6869897c52782771c7c
nikoladavitkovski <82374008+nikoladavitkovski@users.noreply.github.com>
2022-08-09 12:39:51 +0200

    react

---
+++ reactApp/react.js
@@ -0,0 +1,44 @@
+import { Console } from "console";
+import req from "express/lib/request";
+import { TRUE } from "sass";
+
+var username = console.log("Enter a username:",username);
+username = Parse.console(console.log());
+var password = console.log("Enter a password:", password);
+password = Parse.console(console.log());
+username = "ndavitkovski@yahoo.com";
+password = "nikola123";
+if(username.log == true){
+    console.log(length.username);
+    return username;
+} else{
+    username == false;
+    console.log("Username is false within the code");
+    return username == false;
+}
+if(password.log == true){
+    console.log(length.password);
+    array.forEach(element => {
+        console.log(new);
+    });
+    return True;
+}
+else if(password.log == false){
+    console.log(length.toFixed);
+    length.toString(password);
+    console.log(password==false);
+    void password;
+    continue;
+    if (password.True) {
+    resizeTo.call("password","username");    
+    require password;
+    req.get(password == TRUE);
+    print("Password is nikola123");
+    do{
+        console.log("ndavitkovski@yahoo.com");
+        return "ndavitkovski@yahoo.com";
+    }while(password==9) else {
+        return length.toFixed(username);
+        return length.toPrecision(password);
+    }
+}
